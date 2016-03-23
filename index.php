f44db1a0c0d086eb36ed209fddb2d20100a0f57a
Emmanuel ROCHET <E149760H@U-INF-J-C002-30.iut-nantes.univ-nantes.prive>
2016-03-23 08:09:29 +0100

    site

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+  ?>
+
+<!DOTYPE html>
+<html>
+  <head>
+    <meta charset='UTF-8'>
+    <!-- css des frameworks -->
+    <link rel ="stylesheet" href="../css/bootstrap.css">
+    <link rel ="stylesheet" href="../css/bootstrap-theme.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+     <!-- un theme css plutot flat -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/united/bootstrap.min.css" rel="stylesheet" integrity="sha256-oqtj+Pkh1c3dgdH6V9qoS7qwhOy2UZfyVK0qGLa9dCc= sha512-izanB/WZ07hzSPmLkdq82m5xS7EH/qDMgl5aWR37EII+rJOi5c6ouJ3PYnrw6K+DWQcnMZ+nO1NqDr6SBKLBDg==" crossorigin="anonymous">
+
+    <!-- theme optinem -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <!-- framework JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <style>
+    #corps    {width: 1024px;}
+    #titre    {margin-left: -30%; margin-bottom: -2%}
+    #stitre   {margin-left: -10%; margin-top:0;}
+    #tete     {margin-left: -20%;}
+    #img_head {margin-top: -15px;
+               margin-bottom: -15px;
+		margin-left: -15px;
+		width:1022px;}
+    .nav      {color: #523c24;}
+
+    }
+
+    </style>
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" style="margin-top:-50px;height:51px">
+      <div class="container">
+        <a class="navbar-brand"> Belaire</a>
+          <button class="navbar-toggle" data-target="#navbar-main" data-toggle="collapse" type="button">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div id="navbar-main" class="navbar-collapse navbar" style="background-color:#996600;">
+        <ul class="nav navbar-nav">
+          <li><a href=#>Accueil</a></li>
+          <li><a href=#>Chambres</a></li>
+          <li><a href=#>Activités</a></li>
+          <li><a href=#>Actualité</a></li>
+          <li><a href=#>Contact</a></li>
+          <li><a href=#>English website</a></li>
+        </ul>
+      </div>
+    </div>
+
+
+    <center>
+      <div id="corps" style="margin-top:75px; ">
+      <div id="tete">
+        <h1 id="titre"> Bel-Air</h1> </br>
+        <h3 id="stitre"> Chambres d'hôtes<h3>
+      </div>
+
+      <div class="jumbotron">
+            <h2 style="margin-top:-45px">Resultats</h2>
+        <div class="panel-body">
+      <img id="img_head" src="images/photos-aeriennes-et-chambres.jpg">
+    </div>
+      </div>
+    </div>
+    </center>
+  </body>
+</html>
